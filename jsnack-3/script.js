223c198ff14049780acca66a193b6e3fda3c54fe
Robin9800 <robin.costanzo@gmail.com>
2022-03-11 10:21:05 +0100

    jsnack-2 fixed

--- jsnack-3/script.js
+++ jsnack-3/script.js
@@ -1,2 +1,1 @@
-/* Chiedi un numero di 4 cifre all’utente e calcola la somma di tutte le cifre che compongono il numero. 
-(questo potrebbe richiedere un minimo di ricerca) */
+// Chiedi un numero di 4 cifre all’utente e calcola la somma di tutte le cifre che compongono il numero.
