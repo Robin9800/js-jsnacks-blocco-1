9d34b6e8c9fedcd20727add582610936d99845d6
Robin9800 <robin.costanzo@gmail.com>
2022-03-11 10:36:41 +0100

    ciclo while inserito

--- jsnack-2/script.js
+++ jsnack-2/script.js
@@ -5,8 +5,12 @@
  const numbers = [];
 
  /*Creaimo un ciclo in cui chiediamo per 6 volte all’utente di inserire un numero. */
- for(let i = 1; i <= 6; i++){
-    let number = prompt('Inserisci un numero');
+ for(let i = 0; i < 6; i++){
+    let number;
+
+    while(isNaN(number)){
+        number = prompt(`${i+1} Inserisci un numero`);
+    }
     //Se il numero inserito dall'utente è dispari, allora scrivilo in console.
     if(!isNaN(number) && number % 2 !==0){
         numbers.push(number);
--- jsnack-3/script.js
+++ jsnack-3/script.js
@@ -1,1 +1,10 @@
 // Chiedi un numero di 4 cifre all’utente e calcola la somma di tutte le cifre che compongono il numero.
+
+//Dichiariamo una variabile che corrisponde al numero inserito dall'utente.
+let numeroUser;
+
+/*Creiamo un ciclo while che chiede all'utente di inserire un numero di 4 cifre
+finchè l'utente inserisci un valore che non è un numero oppure che non è lungo 4 cifre*/
+while(isNaN(parseInt(numeroUser)) || numeroUser.length !== 4 ){
+    numeroUser = prompt('Inserisci un numero di quattro cifre');
+}
